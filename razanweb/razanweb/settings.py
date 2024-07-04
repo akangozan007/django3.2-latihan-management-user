be3295f3ec57634623793bb73dedbab996795712
root <root@portofolioku.com>
2024-07-04 09:59:24 -0400

    Mengupdate

--- razanweb/razanweb/settings.py
+++ razanweb/razanweb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # menambahkan main app
     'main.apps.MainConfig',
+    # 'main',
     # menambahkan app ekstension UI dari Bootstrap
     'crispy_forms',
     'crispy_bootstrap5'
@@ -123,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # '/var/www/static/',
+    '/home/razan/django3.2-latihan-management-user/razanweb/main/static',
+
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
