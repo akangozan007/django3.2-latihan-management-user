1b80e374acc4cda870659d4839999ec7876ac052
root <root@portofolioku.com>
2024-07-28 10:57:56 -0400

    Mengupdate Dashboard

--- razanweb/main/views.py
+++ razanweb/main/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import logout as auth_logout
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import FormPendaftaran, PostinganForm
 from django.contrib.auth import login as auth_login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from .models import Post
-
+import json
 
 
 def home(request):
@@ -24,21 +24,25 @@ class CustomLoginView(LoginView):
 def login(request):
     return CustomLoginView.as_view()(request)
 
-
 @login_required(login_url="/login/")
 def dashboard(request):
+    posts = Post.objects.all()
     context = {
         'judul': 'Dashboard',
     }
-    if not request.user.is_authenticated:
-        return redirect("/login")
-    else : 
-        post = Post.objects.all()
-        # aksi hapus
-        if request.method == "DELETE":
-            post_id = request.DELETE.get("post-id")
+    
+    if request.method == "POST":
+        post_id = request.POST.get("post-id")
+        post = Post.objects.filter(id=post_id).first()
+        if post and post.author == request.user:
+            post.delete()
+            context['respon'] = post_id
             print(post_id)
-        return render(request, 'main/dashboard.html', {"posts":post})
+    
+    context['posts'] = posts
+    return render(request, 'main/dashboard.html', context)
+   
+
    
 def Logout(request):
     auth_logout(request)
